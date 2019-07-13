bcde44fc439284fc064ca766b763a6a5e5e4292b
Simanta <syed.simanta06@gmail.com>
2019-07-13 14:07:38 +0600

    Updates

---
+++ precache-manifest.a0b455f66a6fc93c985044d03df9454a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a824f110c680deaf2257b05b2a960550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd972bb07bd3763df3e9",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "ab3156c38398abd6dcae",
+    "url": "/static/js/2.bd7d5b5b.chunk.js"
+  },
+  {
+    "revision": "dd972bb07bd3763df3e9",
+    "url": "/static/js/main.94a191f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
